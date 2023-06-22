085235225b5405f2ecf80d9916af2bd45760cf77
OnlyStu <371706801@qq.com>
2023-06-22 10:12:24 +0800

    平方根

--- bin/leet_code.dart
+++ bin/leet_code.dart
@@ -1,5 +1,9 @@
-import 'package:leet_code/src/reverse_words_in_a_string_iii.dart' as reverse_words;
-import 'package:leet_code/src/plu_one.dart' as plu_one;
+
+// ignore: avoid_relative_lib_imports
+import '../lib/src/reverse_words_in_a_string_iii.dart' as reverse_words;
+// ignore: avoid_relative_lib_imports
+import '../lib/src/plu_one.dart' as plu_one;
+import '../lib/src/sqrtx.dart' as sqrtx;
 
 void main(List<String> arguments) {
   // 测试reverseWords
@@ -11,4 +15,8 @@ void main(List<String> arguments) {
   print(plu_one.Solution().plusOne([4,3,2,1]));
   print(plu_one.Solution().plusOne([9,9,9]));
   print(plu_one.Solution().plusOne([8,9,9,9]));
+
+  print(sqrtx.Solution().mySqrt(8));
+  print(sqrtx.Solution().mySqrt(3));
+  print(sqrtx.Solution().mySqrt(1024));
 }
